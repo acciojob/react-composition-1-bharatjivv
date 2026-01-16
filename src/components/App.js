36fab979b7d8a0a459de3cb78f1e1edc529eba9a
Bharat <hibharatj@gmail.com>
2026-01-16 15:54:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,25 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tabs from "./Tabs";
 
-const App = () => {
-  return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+function App() {
+  const tabsData = [
+    {
+      title: "Home",
+      content: <p>Welcome to Home tab</p>,
+    },
+    {
+      title: "Profile",
+      content: <p>This is your profile</p>,
+    },
+    {
+      title: "Settings",
+      content: <p>Change your settings here</p>,
+    },
+  ];
+
+  return <Tabs tabs={tabsData} />;
 }
 
-export default App
+export default App;
